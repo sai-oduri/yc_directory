cc3a0627f18aea68757b97bb13c43d0300a81691
sai-oduri <saioduri95@gmail.com>
2024-11-06 15:04:17 +0530

    realtime update

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,6 @@
 import SearchForm from "@/components/SearchForm";
 import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
-import { client } from "@/sanity/lib/client";
+import { sanityFetch, SanityLive } from "@/sanity/lib/live";
 import { STARTUPS_QUERY } from "@/sanity/lib/queries";
 
 export default async function Home({ searchParams }: {
@@ -9,7 +9,7 @@ export default async function Home({ searchParams }: {
 
   const query = (await searchParams).query;
 
-  const posts = await client.fetch(STARTUPS_QUERY);
+  const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY })
 
   return (
     <>
@@ -41,6 +41,7 @@ export default async function Home({ searchParams }: {
           }
         </ul>
       </section>
+      <SanityLive />
     </>
   );
 }
