29f7c2ce023c6e7b73c306e52660ac607eb5ebe4
sai-oduri <saioduri95@gmail.com>
2024-11-06 14:50:24 +0530

    sanity typegen and groq queries

--- components/StartupCard.tsx
+++ components/StartupCard.tsx
@@ -3,10 +3,13 @@ import { EyeIcon } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import { Button } from "./ui/button";
+import { Author, Startup } from "@/sanity/types";
+
+export type StartupTypeCard = Omit<Startup, "author"> & { author?: Author };
 
 const StartupCard = ({ post }: { post: StartupTypeCard }) => {
 
-  const { _createdAt, description, views, title, category, _id, image, author: { id: authorId, name } } = post;
+  const { _createdAt, description, views, title, category, _id, image, author } = post;
 
   return (
     <li className="startup-card group">
@@ -22,9 +25,9 @@ const StartupCard = ({ post }: { post: StartupTypeCard }) => {
 
       <div className="flex-between mt-5 gap-5">
         <div className="flex-1">
-          <Link href={`/user/${authorId}`}>
+          <Link href={`/user/${author?._id}`}>
             <p className="text-16-medium line-clamp-1">
-              {name}
+              {author?.name}
             </p>
           </Link>
 
@@ -35,7 +38,7 @@ const StartupCard = ({ post }: { post: StartupTypeCard }) => {
           </Link>
         </div>
 
-        <Link href={`/user/${authorId}`}>
+        <Link href={`/user/${author?._id}`}>
           <Image src="https://placehold.co/48x48" alt="placeholder" width={48} height={48} className="rounded-full" />
         </Link>
       </div>
@@ -49,7 +52,7 @@ const StartupCard = ({ post }: { post: StartupTypeCard }) => {
       </Link>
 
       <div className="flex-between gap-3 mt-5">
-        <Link href={`/?query=${category.toLowerCase()}`}>
+        <Link href={`/?query=${category?.toLowerCase()}`}>
           <p className="text-16-medium">{category}</p>
         </Link>
         <Button className="startup-card_btn" asChild>
